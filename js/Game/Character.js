67a0830bfd7753a931852ced303f68288bf872bc
Michael Gilbert <drumz4jah@yahoo.com>
2016-11-29 20:20:56 -0700

    Create character and buttons to move it left and right

--- js/Game/Character.js
+++ js/Game/Character.js
@@ -5,7 +5,7 @@
  * @providesModule
  */
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
     AppRegistry,
     TouchableNativeFeedback,
@@ -16,19 +16,44 @@ import {
     BackAndroid
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Octicons';
+import Matter from 'matter-js';
 
 class Character extends Component {
+
+
+    static contextTypes = {
+        engine: PropTypes.object,
+        scale: PropTypes.number,
+    };
+
     constructor(props) {
         super(props);
     }
 
+    move = (body, x) => {
+        Matter.Body.setVelocity(body, { x, y: 0});
+    };
+
     render() {
+        //let x_pos = this.props.x_pos;
+        let x_pos = this.props.x_pos;
+        let iconStyle = {
+            position: 'absolute',
+            left: x_pos,
+            top: this.props.window.height-200
+        };
+
         return (
             <View>
-                <Icon name="device-mobile" size={50} />
+                <Icon name="device-mobile" size={50} style={iconStyle} />
             </View>
         )
     }
 }
+
+Character.propTypes = {
+    x_pos: PropTypes.number,
+    window: PropTypes.object
+};
 
 export default Character;
--- js/Game/GameScreen.js
+++ js/Game/GameScreen.js
@@ -6,36 +6,41 @@
  * @providesModule
  */
 
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import {
     AppRegistry,
-    TouchableNativeFeedback,
+    TouchableHighlight,
     StyleSheet,
     Text,
     TextInput,
     View,
-    BackAndroid
+    BackAndroid,
+    Dimensions
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import clone from 'clone';
+import { Loop, Stage, World, Body } from 'react-game-kit/native';
+import Matter from 'matter-js';
 
 import Character from './Character';
 
-/**
- * Represents the game home screen
- */
+var window = Dimensions.get('window');
+
+const moveStep = 20;
+
 export class GameScreen extends Component {
+
 
     constructor(props) {
         super(props);
 
         // An event handler for the back button clicked - used by the component mounting and unmounting callbacks
         this.onBackClickedEH = this.onBackClicked.bind(this);
-
         this.difficulty = this.props.route.difficulty;
 
         this.state = {
-            timeRemaining: 0
+            timeUsed: 0,
+            characterXPos: Math.floor(window.width / 2)
         };
     }
 
@@ -44,8 +49,9 @@ export class GameScreen extends Component {
      */
     componentDidMount() {
         BackAndroid.addEventListener("hardwareBackPress", this.onBackClickedEH);
+        window = Dimensions.get('window');
         this.timeInterval = setInterval(() => {
-            this.setState({timeRemaining: this.state.timeRemaining + 1})
+            this.setState({timeUsed: this.state.timeUsed + 1})
         }, 1000);
     }
 
@@ -76,9 +82,9 @@ export class GameScreen extends Component {
     }
 
     timeToString() {
-        if (this.state.timeRemaining < 0)
+        if (this.state.timeUsed < 0)
             return 0;
-        let t = this.state.timeRemaining;
+        let t = this.state.timeUsed;
         let mins = Math.floor(t/60);
         if (mins > 0) {
             t -= mins*60;
@@ -93,20 +99,39 @@ export class GameScreen extends Component {
         }
     }
 
-    getPageTitle() {
-        let diff = this.difficulty.charAt(0).toUpperCase() + this.difficulty.slice(1);
-        return diff + " mode";
+    physicsInit = (engine) => {
+        const ground = Matter.Bodies.rectangle(
+            0, window.height-20,
+            window.width, 100,
+            {
+                isStatic: true,
+            },
+        );
+
+        Matter.World.addBody(engine.world, ground);
+    };
+
+    onLeftPressed() {
+        let new_x = this.state.characterXPos - moveStep;
+        if (new_x < 0)
+            new_x = 0;
+
+        this.setState({
+            characterXPos: new_x
+        });
     }
 
-    /**
-     * Render the component
-     */
-    render() {
-        let currentAnagram = null;
-        if (this.state.questionNumber >= 0) {
-            currentAnagram = this.state.anagrams[this.state.questionNumber].question;
-        }
+    onRightPressed() {
+        let new_x = this.state.characterXPos;
+        if (new_x < window.width - 30)
+            new_x += moveStep;
 
+        this.setState({
+            characterXPos: new_x
+        });
+    }
+
+    render() {
         let timerStyle = {
             textAlign: "right",
             fontSize: 20,
@@ -114,17 +139,38 @@ export class GameScreen extends Component {
             color: "green"
         };
 
-        if (this.state.timeRemaining > 60) {
+        if (this.state.timeUsed > 60 && this.state.timeUsed < 120) {
             timerStyle.color = "#cc0"
-        } else if (this.state.timeRemaining > 120) {
+        } else if (this.state.timeUsed > 120) {
             timerStyle.color = "red";
         }
 
-        return (
+        /*return (
             <View style={styles.container}>
                 <Text style={timerStyle}>{this.timeToString()}</Text>
                 <Character />
             </View>
+        );*/
+        return (
+            <Loop>
+                <Stage height={window.height} width={window.width}>
+                    <World
+                        onInit={this.physicsInit}>
+                        <TouchableHighlight
+                            onPress={this.onLeftPressed.bind(this)}
+                            style={styles.leftButton}>
+                            <Text style={{color: 'white'}}>Left</Text>
+                        </TouchableHighlight>
+                        <TouchableHighlight
+                            onPress={this.onRightPressed.bind(this)}
+                            style={styles.rightButton}>
+                            <Text style={{color: 'white'}}>Right</Text>
+                        </TouchableHighlight>
+                        <Character x_pos={this.state.characterXPos} window={window} />
+                        <Text style={timerStyle}>{this.timeToString()}</Text>
+                    </World>
+                </Stage>
+            </Loop>
         );
     }
 }
@@ -140,36 +186,26 @@ const styles = StyleSheet.create({
         alignItems: 'stretch',
         backgroundColor: '#F5FCFF',
     },
-    container2: {
-        flex: 1,
-        justifyContent: 'space-between',
-        alignItems: 'stretch',
-        backgroundColor: '#F5FCFF',
-        paddingTop: 20
+    leftButton: {
+        position: 'absolute',
+        bottom: 25,
+        width: 75,
+        height: 75,
+        backgroundColor: 'blue'
     },
-    toolbar: {
-        height: 56,
-        backgroundColor: '#e9eaed'
+    rightButton: {
+        position: 'absolute',
+        bottom: 25,
+        right: 0,
+        width: 75,
+        height: 75,
+        backgroundColor: 'blue'
     },
-    wideButton: {
-        backgroundColor: '#1FB6FF',
-        margin: 10,
-        marginTop: 0,
-        marginBottom: 30,
-        padding: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    wideButtonText: {
-        fontSize: 20
-    },
-    guessText: {
-        marginLeft: 10
-    },
-    youredone: {
-        fontSize: 40,
-        textAlign: "center"
+    character: {
+        position: 'absolute',
+        bottom: 0
     }
+
 });
 
 export default GameScreen;
